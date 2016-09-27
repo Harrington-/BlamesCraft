df8eb8d2ad5d8e8d2c04c28963845a899ff60c6b
Blake <blake.harrington14@gmail.com>
2016-09-26 23:44:31 -0400

    The beginning of BlamesCraft!
    Everything we've done thus far

---
+++ Blames Craft/Assets/Terrain/Scripts/Chunk.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public class Chunk : MonoBehaviour {
+
+	public int x;
+	public int y;
+	public GenerateTerrain Parent;
+	public Guid ChunkId;
+	public static Dictionary<string, List<GameObject>> availableBlocks = new Dictionary<string, List<GameObject>>();
+
+	public List<GameObject> blocks;
+
+	public Chunk(GenerateTerrain parent, int X, int Y)
+	{
+		blocks = new List<GameObject>();
+		ChunkId = Guid.NewGuid();
+		Parent = parent;
+		x = X;
+		y = Y;
+		loadChunk(x, y);
+
+	}
+	void loadChunk(int chunkX, int chunkY)
+	{
+		for (int x = 0; x < Parent.chunkSize; x++)
+		{
+			for (int y = 0; y < Parent.chunkSize; y++)
+			{
+				int c = Parent.standardizedStartingY + (int)(Mathf.PerlinNoise((Parent.seed + (chunkX * Parent.chunkSize) + x) / Parent.scale, (Parent.seed + (chunkY * Parent.chunkSize) + y) / Parent.scale) * Parent.generationHeight);
+				Vector3 blockPos = new Vector3((chunkX * Parent.chunkSize) + x, c, (chunkY * Parent.chunkSize) + y);
+				blocks.Add(RequestBlock(Parent.grass_block, blockPos));
+			}
+		}
+	}
+
+	public bool isBounds(int playerx, int playery)
+	{
+		return (int)(playerx / Parent.chunkSize) == x && (int)(playery / Parent.chunkSize) == y;
+    }
+
+	public void Clear()
+	{
+		foreach (GameObject obj in blocks)
+		{
+			obj.SetActive(false);
+			//if (availableBlocks.ContainsKey(obj.tag))
+			//{
+			//	availableBlocks[obj.tag].Add(obj);
+			//}
+			//else
+			//{
+			//	availableBlocks.Add(obj.tag, new List<GameObject>() { obj });
+			//}
+		}
+		blocks.Clear();
+	}
+
+	public GameObject RequestBlock(GameObject preFab, Vector3 position)
+	{
+		if (availableBlocks.ContainsKey(preFab.tag))
+		{
+			var matches = availableBlocks[preFab.tag];
+			if (matches.Count() > 0)
+			{
+				var perfectMatch = matches.FirstOrDefault(x => x.transform.position == position);
+				if (perfectMatch != null)
+				{
+					perfectMatch.SetActive(true);
+					matches.Remove(perfectMatch);
+					return perfectMatch;
+				}
+				var returnData = matches.First();
+				returnData.SetActive(true);
+				returnData.transform.position = position;
+				matches.Remove(returnData);
+				return returnData;
+			}
+		}
+		if (availableBlocks.Any() && availableBlocks.First().Value.Any())
+		{
+			var anyBlock = availableBlocks.First().Value.First();
+			anyBlock.SetActive(true);
+			anyBlock.transform.position = position;
+			availableBlocks.First().Value.Remove(anyBlock);
+			return anyBlock;
+		}
+		return (GameObject)Instantiate(preFab, position, Quaternion.identity);
+	}
+}
---
+++ Blames Craft/Assets/Terrain/Scripts/GenerateTerrain.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GenerateTerrain : MonoBehaviour
+{
+
+
+	[Tooltip("Larger This Height the More Profoud The Hills Will Be")]
+	public int generationHeight = 32;
+
+	[Tooltip("Total Blocks Rendered in Each Chunk")]
+	public int chunkSize = 16;
+
+	[Tooltip("Total Chunks Rendered in Each Direction")]
+	public int renderDistance = 3;
+
+	[Tooltip("General Starting Y for the Map")]
+	public int standardizedStartingY = 60;
+
+	[Tooltip("Lower The Value the More Crazy it Gets")]
+	public float scale = 1;
+
+	[Tooltip("Lower The Value the More Crazy it Gets")]
+	public float seed = 1;
+
+	List<Chunk> chunks;
+	private bool chunkChanged;
+	public GameObject dirt_block;
+	public GameObject sand_block;
+	public GameObject grass_block;
+	public GameObject stone_block;
+	private GameObject player;
+	private System.Guid CurrentChunk;
+
+	public int playerChunkX;
+	public int playerChunkY;
+
+	// Use this for initialization
+	void Start()
+	{
+		seed = (int)Random.Range(0, 99999999);
+		
+		player = GameObject.FindGameObjectWithTag("Player");
+		chunks = new List<Chunk>();
+		for (int i = -renderDistance; i < renderDistance; i++)
+		{
+			for (int u = -renderDistance; u < renderDistance; u++)
+			{
+				chunks.Add(new Chunk(this, i, u));
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		checkPlayerChunkPosition();
+		if (chunkChanged)
+		{
+			reloadChunk();
+		}
+	}
+
+	void checkPlayerChunkPosition()
+	{
+		var x = (int)player.transform.position.x;
+		var y = (int)player.transform.position.z;
+		var chunk = chunks.FirstOrDefault(c => c.isBounds(x, y));
+		if (chunk.ChunkId != CurrentChunk)
+		{
+			CurrentChunk = chunk.ChunkId;
+			chunkChanged = true;
+		}
+	}
+
+	void reloadChunk()
+	{
+		var chunk = chunks.Single(X => X.ChunkId == CurrentChunk);
+		var tempList = new List<Vector2>();
+		for (int i = -renderDistance; i < renderDistance; i++)
+		{
+			for (int u = -renderDistance; u < renderDistance; u++)
+			{
+				tempList.Add(new Vector2(chunk.x + u, chunk.y + i));
+			}
+		}
+		clearChunks(tempList);
+		chunkChanged = false;
+	}
+
+	void clearChunks(List<Vector2> neededChunks)
+	{
+		var newChunks = new List<Chunk>();
+		foreach (Chunk chunkie in chunks)
+		{
+			if(!neededChunks.Any(x=>x.x == chunkie.x && x.y == chunkie.y))
+			{
+				chunkie.Clear();
+			}
+			else
+			{
+				newChunks.Add(chunkie);
+				neededChunks.Remove(neededChunks.First(x => x.x == chunkie.x && x.y == chunkie.y));
+			}
+		}
+		foreach(var item in neededChunks)
+		{
+			newChunks.Add(new Chunk(this, (int)item.x, (int)item.y));
+		}
+		chunks = newChunks;
+	}
+
+	
+}
